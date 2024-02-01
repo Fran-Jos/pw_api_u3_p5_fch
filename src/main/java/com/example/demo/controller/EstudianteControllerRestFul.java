25fde6c1dc8a4f9d30b902960cd6a2cd5436e5d5
FranJos <fjchanataxi@uce.edu.ec>
2024-01-31 19:39:10 -0500

    consumos de postman

---
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
+
+//Una API puede tener muchos servicios
+//API: determinada por el proyecto Java
+//Un servicio tiene muchas capacidades
+//Con una clase controller se implementa un controlador rest en una API
+//Servicio -> Controller: Clase controller
+
+@RestController // Servicio
+@RequestMapping(path = "/estudiantes")
+
+public class EstudianteControllerRestFul {
+
+	@Autowired
+	private IEstudianteService estudianteService;
+
+	// Métodos: Capacidades
+	@GetMapping(path = "/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(10);
+	}
+
+	@PostMapping(path = "/guardar")
+	public void guardar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
+	}
+
+	@PutMapping(path = "/actualizar")
+	public void actualizar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.actualizar(estudiante);
+	}
+
+    @PatchMapping(path ="/actualizarParcial" )
+	public void actualizarParcial(@RequestBody Estudiante estudiante){
+
+		this.estudianteService.actualizarParcial(estudiante.getApellido(),estudiante.getNombre(), estudiante.getId());
+	}
+
+	@DeleteMapping(path = "/borrar")
+	public void borrar(){
+		this.estudianteService.eliminar(10);
+	}
+}
