d5812f57951f868254f5615967ee67d842aef1a3
Franjos” <fjchanataxi@uce.edu.ec>
2024-02-10 10:46:39 -0500

    api completa

---
+++ src/main/java/com/example/demo/service/IEstudianteService.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.to.EstudianteTO;
+
+public interface IEstudianteService {
+	// CRUD
+	// Create, Read, Uopdate, Delete
+	public void guardar(Estudiante estudiante);
+
+	public void actualizar(Estudiante estudiante);
+
+	public void actualizarParcial(String apellido, String nombre, Integer id);
+
+	public Estudiante buscar(Integer id);
+	
+	public EstudianteTO buscarTO(Integer id);
+	
+	public List<Estudiante> buscarTodos(String genero);
+	
+	public List<EstudianteTO> buscarTodosTO();
+
+	public void eliminar(Integer id);
+}
